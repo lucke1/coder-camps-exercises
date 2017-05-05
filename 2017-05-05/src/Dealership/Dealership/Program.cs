9dc835450d20aa15497a9ef9eb73452656a6c54a
Jason Tennant <xcodula@icloud.com>
2017-05-05 15:27:37 -0700

    2017-05-05

---
+++ 2017-05-05/src/Dealership/Dealership/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dealership
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new VehicleService();
+            var list = service.GetVehicles();
+            foreach (var item in list)
+            {
+                // do stuff
+            }
+        }
+    }
+}
---
+++ 2017-05-05/src/Dealership/Dealership/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dealership
+{
+    class Vehicle
+    {
+        public int VehicleId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public DateTime Year { get; set; }
+    }
+}
